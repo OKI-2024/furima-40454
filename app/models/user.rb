e0597a40ae38fafce07a8195fb8fb3c3a106cdad
OKI-2024 <tetsuokinokun@gmail.com>
2024-03-20 21:55:50 +0900

    バリデーションの修正（削除）
    emailとpasswordのバリデーションを削除
    理由：deviseにデフォルトで備わっていた為

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,6 @@ class User < ApplicationRecord
   
   validates :nickname, presence: true
   
-  validates :email, presence: true,
-            format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "は有効な形式で入力してください" }
-  
-  validates :password, presence: true,
-            length: { minimum: 6 },
-            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "は半角英数字の混合で入力してください" } 
-
   validates :first_name, presence: true,
             format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角文字で入力してください" }
 
