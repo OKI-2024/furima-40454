aad92d6a32fcffe70431b2fc3ee230bf22b97f7c
OKI-2024 <tetsuokinokun@gmail.com>
2024-03-20 18:37:23 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,32 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-
   
+  validates :nickname, presence: true
+  
+  validates :email, presence: true,
+            format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "は有効な形式で入力してください" }
+  
+  validates :password, presence: true,
+            length: { minimum: 6 },
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "は半角英数字の混合で入力してください" } 
 
+  validates :first_name, presence: true,
+            format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角文字で入力してください" }
+
+  validates :last_name, presence: true,
+            format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角文字で入力してください" }
+
+  validates :first_name_kana, presence: true,
+            format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+
+  validates :last_name_kana, presence: true,
+            format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+
+
+
+
+  validates :birthday, presence: true
 
 
   
