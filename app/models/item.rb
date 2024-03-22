7dd3078162a5d742366798d1164702473ff79487
OKI-2024 <tetsuokinokun@gmail.com>
2024-03-22 22:25:51 +0900

    itemモデルのアソシエーション記述（画像以外）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :purchase_record
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_charge
+  belongs_to :region
+  belongs_to :days_up_to_delivery
+  
+
+
+
+
+
+
 end
