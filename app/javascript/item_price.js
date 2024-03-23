89970b432623cd6e756c06a671223d10306e40f3
OKI-2024 <tetsuokinokun@gmail.com>
2024-03-23 18:19:50 +0900

    JSによる手数料・収益表示設定

---
+++ app/javascript/item_price.js
@@ -0,0 +1,11 @@
+window.addEventListener('turbo:load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1).toLocaleString();
+    const addProfitDom = document.getElementById("profit");
+    addProfitDom.innerHTML = Math.floor(inputValue * 0.9).toLocaleString();
+  })
+})
