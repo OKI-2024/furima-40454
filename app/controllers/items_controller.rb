a961f0eb64354a66d487abaf330978c0adcd8dc8
OKI-2024 <tetsuokinokun@gmail.com>
2024-03-26 17:48:11 +0900

    items#destroyを編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,8 +37,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if @item.user == current_user
+       @item.destroy
+       redirect_to root_path
+    else
+      redirect_to item_path(@item)
   end
 
   private
