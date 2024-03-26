38ee31457b79cff2efeaae4b95e177b6aaf10638
OKI-2024 <tetsuokinokun@gmail.com>
2024-03-26 22:40:15 +0900

    購入ボタンのパスと購入コントローラーの作成

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,10 +1,17 @@
 class PurchaseRecordsController < ApplicationController
 
 def index
-  
+  @purchase_record = PurchaseRecord.new
 end
 
 def create
+  @purchase_record = PurchaseRecord.new(purchase_record_params)
+  if @purchase_record.valid?
+     @purchase_record.save
+     return redirect_to root_path
+  else
+    render 'index', status: :unprocessable_entity
+  end
 end
 
 
