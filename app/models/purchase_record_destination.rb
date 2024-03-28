e6c5d93927d784d2dbecae335d4e0328fa070234
OKI-2024 <tetsuokinokun@gmail.com>
2024-03-28 17:58:13 +0900

    バリデーション（エラーメッセージ）

--- app/models/purchase_record_destination.rb
+++ app/models/purchase_record_destination.rb
@@ -3,22 +3,40 @@ class PurchaseRecordDestination
   attr_accessor :postal_code, :region_id, :city, :block, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
     validates :block
-    validates :phone_number, format: {with: /\A\d{10,11}\z/ , message: "is invalid."}
+    validates :phone_number
     validates :region_id, numericality: { other_than: 1, message: "can't be blank" }
-
+    
     validates :user_id
     validates :item_id
     
     validates :token
     end
+
+    validate :validate_phone_number_format
     
     
   def save
     purchase_record = PurchaseRecord.create(user_id: user_id, item_id: item_id)
     Destination.create(postal_code: postal_code, region_id: region_id, city: city, block: block, building: building, phone_number: phone_number, purchase_record_id: purchase_record.id )
   end
+
+  private
+
+  def validate_phone_number_format
+    return if phone_number.blank? # 空の場合
+
+    unless phone_number.match?(/\A\d+\z/) # 記号が含まれる場合
+      errors.add(:phone_number, "Phone number is invalid. Input only number")
+    end
+    
+    unless phone_number.length >= 10 # 10桁未満の場合
+      errors.add(:phone_number, "Phone number is too short")
+    end
+  end
+
 
 end
