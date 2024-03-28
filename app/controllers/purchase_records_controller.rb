dae221d83fbe9676c6eec374f5220a4d0d42ba24
OKI-2024 <tetsuokinokun@gmail.com>
2024-03-28 19:12:28 +0900

    売り切れ時の表示＆ページ遷移設定②

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,9 +1,15 @@
 class PurchaseRecordsController < ApplicationController
+  before_action :authenticate_user!
 
 def index
-  gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
-  @item = Item.find(params[:item_id])
-  @purchase_record_destination = PurchaseRecordDestination.new
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    @item = Item.find(params[:item_id])
+  
+  if @item.purchase_record.nil? && @item.user != current_user
+     @purchase_record_destination = PurchaseRecordDestination.new
+  else
+     redirect_to root_path
+  end
 end
 
 def new
@@ -24,23 +30,12 @@ def create
   end
 end
 
-  #@purchase_record = PurchaseRecord.new(purchase_record_params)
-  #if @purchase_record.valid?
-     #pay_item
-     #@purchase_record.save
-     #return redirect_to root_path
-  #else
-    #gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
-    #render 'index', status: :unprocessable_entity
-  #end
-
-
 private
 
 
 def purchase_record_destination_params
   params.require(:purchase_record_destination).permit(:postal_code, :region_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
-  end
+end
 
 def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
